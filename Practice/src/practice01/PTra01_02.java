78ad5002bf239c735de3ee2174911b627f7771d1
DESKTOP-KCEOEF0\likeit_student <sorahiromatugawa@gmail.com>
2018-02-09 18:33:37 +0900

    Ptra10_05まで完了

--- Practice/src/practice01/PTra01_02.java
+++ Practice/src/practice01/PTra01_02.java
@@ -9,10 +9,9 @@ package practice01;
 public class PTra01_02 {
 	public static void main(String[] args) {
 
-		// ★ 12～14行目をコメントにしてください。
-		ここをコメントにしてください
-		ここをコメントにしてください
-		ここをコメントにしてください
+	    System.out.println("Hello,world");
+		System.out.println("Hello,world");
+		System.out.println("Hello,world");
 
 		//  ★「Hello, world」と出力（コマンドプロンプトに表示）してください
 
